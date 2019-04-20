854cca1c81da5b1ce5fc1cb1ca704605f6a7c86f
Adam Townsend <adam@desnudopenguino.com>
2019-04-20 14:06:38 -0700

    added a couple git macros and shortened a few key commands to keep the formats cleaner

--- keyboards/gh60/keymaps/desnudopenguino/keymap.c
+++ keyboards/gh60/keymaps/desnudopenguino/keymap.c
@@ -1,9 +1,23 @@
 #include QMK_KEYBOARD_H
 
+/* LAYER NAME VARIABLES */
 #define _DV 0
 #define _QW 1
 #define _GM 2
 #define _MO 3
+
+/* KEY SHORTNAMES */
+#define CTL_ESC CTL_T(KC_ESC)
+#define LALT_CB LALT_T(KC_LCBR)
+#define RALT_CB RALT_T(KC_RCBR)
+#define G_MOD   OSL(_GM)
+
+/* custom keycodes for macros */
+enum custom_keycodes {
+ G_ADD = SAFE_RANGE,
+ G_ADALL
+};
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -11,21 +25,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LBRC, KC_RBRC, KC_BSPC,
     KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, KC_EQL,  KC_BSLS,
-    CTL_T(KC_ESC), KC_A, KC_O, KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, KC_NO,   KC_ENT,
+    CTL_ESC, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, KC_NO,   KC_ENT,
     KC_LSPO, KC_NO,   KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_NO,   KC_RSPC,
-    KC_CAPS, KC_LGUI,  LALT_T(KC_LCBR),           KC_SPC,                             KC_NO,  MO(_MO), RALT_T(KC_RCBR), KC_APP, KC_RCTL
+    KC_CAPS, KC_LGUI, LALT_CB,                   KC_SPC,                             KC_NO,   MO(_MO), RALT_CB, G_MOD,   KC_RCTL
   ),
 
   /* 1: qwerty */
   LAYOUT(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-    KC_CAPS, KC_A, KC_S, KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_ENT,
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NO,   KC_ENT,
     KC_LSPO, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_NO,   KC_RSPC,
-    KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_NO,   MO(_MO), KC_RALT, KC_APP, KC_RCTL
+    KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_NO,   MO(_MO), KC_RALT, KC_APP,  KC_RCTL
   ),
   /* 2: git macros */
-
+  LAYOUT(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, G_ADALL, _______, _______, _______,
+    _______, G_ADD,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______,                   _______,                            _______, _______, _______, _______, _______
+  ),
   /* 3: fn */
   LAYOUT(
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
@@ -75,15 +95,17 @@ void matrix_scan_user(void) {
 
 };
 
-/*
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
     switch(keycode) {
       case G_ADD:
         SEND_STRING("git add ");
-        layer_off(_MC);
+        return false;break;
+      case G_ADALL:
+        SEND_STRING("git add .\n");
         return false;break;
     }
   }
+  return true;
 };
-  */
